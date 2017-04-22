883b52d4622c5d3bc14ec397586d3db6a39498e6
Tusicka <tusicka.iyy@gmail.com>
2017-04-22 20:36:31 +0300

    commit

---
+++ Assets/Scripts/script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) ;
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.U)) ;
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.P)) ;
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.V)) ;
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.Space)) ;
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            transform.position = position;
+        }
+
+    }
+}
